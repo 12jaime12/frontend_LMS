9da9ea5b3a3de8fc850affc2443927a2410e8eb4
Samuelson23 <3samuelmg@gmail.com>
2023-06-19 16:05:10 +0200

    user services

--- src/service/API_proyect/user.service.js
+++ src/service/API_proyect/user.service.js
@@ -1,3 +1,4 @@
+import { updateToken } from "../../util/updateToken";
 import { API } from "./service.config";
 
 //------------------register-------------------------
@@ -11,3 +12,60 @@ export const registerUser = async (formData) => {
     });
 };
 //---------------------verify-code-------------------
+export const checkCode = async (formData)=>{
+  return API.post("/user/checkCode",formData).then((res)=>res).catch((error)=>{return error})
+}
+//---------------------resend-code-------------------
+export const resendCode = async(formData)=>{
+  return API.post("/user/resendCode",formData).then((res)=>res).catch((error)=>{return error})
+}
+//---------------------autologin---------------------
+export const autologinUser = async (formData)=>{
+  return API.post("/user/autologin", formData).then((res)=>res).catch((error)=>{return error})
+}
+//---------------------login-------------------------
+export const loginUser = async (formData)=>{
+  return API.post("/user/login",formData).then((res)=>res).catch((error)=>{return error})
+}
+//---------------------sendPassword------------------
+export const sendPassword = async(formData)=>{
+  return API.patch("/user/sendPassword/:id",formData).then((res)=>res).catch((error)=>{return error})
+}
+//---------------------changePassword----------------
+export const changePassword = async(formData)=>{
+  return API.patch("/user/changePassword", formData,{
+    headers: {
+      Authorization:`Bearer ${updateToken}`
+    }
+  }).then((res)=>res).catch((error)=>{return error})
+}
+//---------------------forgotPassword----------------
+export const forgotPassword=async(formData)=>{
+  return API.patch("/user/forgotPassword", formData).then((res)=>res).catch((error)=>{return error})
+}
+//---------------------updateUser--------------------
+export const updateUser = async(formData)=>{
+  return API.patch("/user/updateUser)",formData,{
+    headers: {
+      Authorization:`Bearer ${updateToken}`
+    }
+  }).then((res)=>res).catch((error)=>{return error})
+}
+//---------------------deleteUser--------------------
+export const deleteUser = async()=>{
+  return API.delete("/user/deleteUser",{
+    headers: {
+      Authorization:`Bearer ${updateToken}`
+    }
+  }).then((res)=>res).catch((error)=>{return error})
+}
+//---------------------getAllUser--------------------
+export const getAllUser = async() =>{
+  return API.get("/user/").then((res)=>res).catch((error)=>{return error})
+}
+//---------------------getByIdUser-------------------
+export const getByIdUser=async()=>{
+  return API.get("user/:id").then((res)=>res).catch((error)=>{return error})
+}
+
+
