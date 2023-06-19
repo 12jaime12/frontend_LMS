3b3fbe99d0dbb1cc29d6c5c08d3e46fdd3cc4b88
12jaime12 <12jaimejcc12@gmail.com>
2023-06-19 16:07:58 +0200

    dia 1 front

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { createContext, useContext, useMemo, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const authContext = () => {
-  return <div>authContext</div>;
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  //--------------------Login---------------------------
+  const [user, setUser] = useState(() => {
+    const data = localStorage.getItem("user");
+    const parseUser = JSON.parse(data);
+
+    if (data) {
+      return parseUser;
+    } else {
+      return null;
+    }
+  });
+
+  const [allUser, setAllUser] = useState({
+    data: {
+      user: {
+        password: "",
+        email: "",
+      },
+    },
+  });
+
+  const bridgeData = (state) => {
+    const data = localStorage.getItem("data");
+    const dataJson = JSON.parse(data);
+    switch (state) {
+      case "ALLUSER":
+        setAllUser(dataJson);
+        localStorage.removeItem("data");
+        break;
+
+      default:
+        break;
+    }
+  };
+  const userlogin = (data) => {
+    localStorage.setItem("user", data);
+    const parseData = JSON.parse(data);
+    setUser(() => parseData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+    navigate("/login");
+  };
+
+  const value = useMemo(
+    () => ({
+      user,
+      setUser,
+      userlogin,
+      allUser,
+      setAllUser,
+      bridgeData,
+      logout,
+    }),
+    [user, allUser]
+  );
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
-export default authContext;
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
