a26992aabc57abf0c8635f5343c8bdf14b27524f
12jaime12 <12jaimejcc12@gmail.com>
2023-06-19 15:40:25 +0200

    dia 1 front

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,30 @@
+import { useNavigate } from "react-router";
 import "./Home.css";
 
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  return (
+    <div className="fondo">
+      <div className="homeContainer">
+        {user !== null ? (
+          <h1 className="titleHome">Bienvenido {user.user}</h1>
+        ) : (
+          <>
+            <h1 className="titleHome">
+              Por favor, regístrese para poder acceder a la página
+            </h1>
+            <button className="btn" onClick={() => navigate("/login")}>
+              IR A LOGIN
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
