f0cffd86b06e53f872e3cf8724ef0371ec91d4f8
Samuelson23 <3samuelmg@gmail.com>
2023-06-19 22:12:12 +0200

    register y useError

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -32,7 +32,7 @@ export const AuthContextProvider = ({ children }) => {
     switch (state) {
       case "ALLUSER":
         setAllUser(dataJson);
-        localStorage.removeItem("data");
+        //localStorage.removeItem("data");
         break;
 
       default:
--- src/hooks/useError/useRegisterError.jsx
+++ src/hooks/useError/useRegisterError.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const useRegisterError = (res, setRegisterOk) => {
-    if(res.status==200){
+    if(res?.status==200){
         console.log("RESSS", res?.data?.user)
-        localStorage.setItem("data", JSON.stringify(res))
+        localStorage.setItem("data", JSON.stringify(res?.data?.user))
         setRegisterOk(()=>true)
     }
 }
