b1c0fee4b3482644b6e10140a1b3f4b99bebf42b
12jaime12 <12jaimejcc12@gmail.com>
2023-06-20 18:18:53 +0200

    dia 5

--- src/pages/Catalogo/Catalogo.jsx
+++ src/pages/Catalogo/Catalogo.jsx
@@ -6,7 +6,9 @@ const Catalogo = () => {
       <h1>Catalogo</h1>
       <div className="marcas">
         <ul>
-          <li></li>
+          <li>
+            <img src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1687275029/HD-wallpaper-bmw-new-logo-bmw-bmw-2020-bmw-cars-bmw-logo-car-new-logo_wgrr3k.jpg" />
+          </li>
           <li></li>
         </ul>
       </div>
