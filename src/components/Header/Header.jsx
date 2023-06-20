f490bdc4616e3d2f67c85acb90999d4c8f4dd080
12jaime12 <12jaimejcc12@gmail.com>
2023-06-20 14:43:52 +0200

    dia 5

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,17 @@ import "./Header.css";
 import React from "react";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <div>
+        <img
+          src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1686737065/logoredondo-removebg-preview_uidph4.png"
+          alt="logo"
+          className="logo"
+        />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
