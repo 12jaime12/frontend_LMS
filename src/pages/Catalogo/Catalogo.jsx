2b69cb6159a754cb00b03fc5c2a1220bbc0d35d9
12jaime12 <12jaimejcc12@gmail.com>
2023-06-20 17:34:37 +0200

    dia 5

--- src/pages/Catalogo/Catalogo.jsx
+++ src/pages/Catalogo/Catalogo.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import "./Catalogo.css";
 const Catalogo = () => {
-  return <div>Catalogo</div>;
+  return (
+    <div className="catalogo">
+      <h1>Catalogo</h1>
+      <div className="marcas">
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Catalogo;
