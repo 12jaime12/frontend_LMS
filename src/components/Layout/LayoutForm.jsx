2ec4e98ef526014675a45fd1893549f75dbba20c
Laura Martínez <you@example.com>
2023-06-20 17:11:51 +0200

    diseñoLOGIN

---
+++ src/components/Layout/LayoutForm.jsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+const FlexForm = styled.div`
+display: flex;
+flex-direction: ${({ direction }) => direction};
+justify-content: center;
+align-items: center;
+background: #f2f2f2;
+width:  ${({ width }) => width};
+heigth: ${({ heigth }) => heigth};
+padding: ${({ padding }) => padding};
+border-radius: 5px;
+gap: ${({ gap }) => gap};
+`;
+export const LayoutForm = ({children, direction, padding, width, heigth, gap}) => {
+  return (
+    <FlexForm direction = { direction} gap={ gap} width={width} heigth={heigth} padding = { padding}>
+        {children}
+    </FlexForm>
+  )
+}
