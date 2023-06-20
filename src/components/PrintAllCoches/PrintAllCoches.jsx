a5a1d8b4425ae3a0a6e705f7986d0d688be44e21
Samuelson23 <3samuelmg@gmail.com>
2023-06-20 18:17:59 +0200

    print coches

--- src/components/PrintAllCoches/PrintAllCoches.jsx
+++ src/components/PrintAllCoches/PrintAllCoches.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import "./PrintAllCoches.css"
 const PrintAllCoches = ({data}) => {
     console.log(data)
   return (
-        <div>{data?.data?.map((elem)=>(
-            <figure key={elem._id}>
+        <div className="divAllCoches">{data?.data?.map((elem)=>(
+            <figure key={elem._id} className="figureCoche">
+              <div className="divFigureCoche">
               <img src={elem.image[0]} alt={elem.modelo}/>
-              <div>
-                <p>{elem.marca}</p>
-                <p>{elem.modelo}</p>
+              <div className="divCocheInfo">
+                <p>{elem.marca} {elem.modelo}</p>
+                <p>Precio: <strong>{elem.precio} €</strong></p>
+              </div>
               </div>
             </figure>
         ))
