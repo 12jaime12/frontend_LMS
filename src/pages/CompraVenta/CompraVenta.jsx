9eee8d6536ee7bcdff815abbcb619967da821b8c
Samuelson23 <3samuelmg@gmail.com>
2023-06-21 11:38:06 +0200

    header -> icono perfil

--- src/pages/CompraVenta/CompraVenta.jsx
+++ src/pages/CompraVenta/CompraVenta.jsx
@@ -3,21 +3,20 @@ import { Outlet, useNavigate } from "react-router";
 import { useAuth } from "../../contexts/authContext";
 import { getAllCochesOcasion } from "../../service/API_proyect/coche.service";
 import Button from "../../components/ui/Button";
-
+import "./Compraventa.css"
 const CompraVenta = () => {
   //AQUI CREO LA ESTRUCTURA DE LA PAGINA DE COMPRAVENTA -> 2 BOTONES CON comprar y vender Y UN OUTLET CON EL CONTENIDO(todos los coches)
   //form comprar, form vender...etc
   const navigate=useNavigate()
   return (
   <>
-  <ul>
-    <li>
+  <nav className="ulNavCompraventa">
+
     <Button type="text" text="COMPRAR" variant="contained" color="white"/>
-    </li>
-    <li>
+
     <Button type="text" text="VENDER" variant="contained" color=""/>
-    </li>
-  </ul>
+
+  </nav>
   <Outlet/>
   </>
   )
