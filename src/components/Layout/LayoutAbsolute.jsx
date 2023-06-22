ea63476b1dc7548eee992d163dc738a42b71009b
Laura Martínez <you@example.com>
2023-06-22 12:33:03 +0200

    dashboardYLayouts

--- src/components/Layout/LayoutAbsolute.jsx
+++ src/components/Layout/LayoutAbsolute.jsx
@@ -5,15 +5,19 @@ const PositionAbsStyled = styled.div`
 background-color: ${({ color }) => color};
 width: 40vw;
 display: flex;
-flex-direction: column;
+flex-direction: ${({direction}) => direction};
 align-items: center;
-padding: 2rem;
-z-index:2;
+z-index:${({zindex})=> zindex};
 position: absolute;
+
+top: ${({top})=>top};
+bottom:${({bottom})=>bottom};
+right: ${({right})=>right};
+left: ${({left})=> left}
 `;
-export const LayoutAbsolute = ({children, color}) => {
+export const LayoutAbsolute = ({children, color, direction, zindex, top, bottom, right, left}) => {
   return (
-    <PositionAbsStyled color={color}>
+    <PositionAbsStyled color={color} direction={direction} z-index={zindex} top={top} bottom={bottom} right={right} left={left}>
         {children}
     </PositionAbsStyled>
 
