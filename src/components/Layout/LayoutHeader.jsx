82231510562adc79af9380c5a6addca9be88aa9a
andreafcb1 <andrea_garcillan@hotmail.com>
2023-06-23 14:21:13 +0200

    sos

--- src/components/Layout/LayoutHeader.jsx
+++ src/components/Layout/LayoutHeader.jsx
@@ -6,13 +6,8 @@ display: flex;
 flex-direction: row;
 justify-content: space-between;
 align-items: center;
-background: rgba(0, 0, 0, 0.33);
 font-size: 20px;
-gap: 4rem;
-margin-right: 20px;
-width:  100vw;
-heigth: 13vh;
-
+height: 10vh;
 `;
 export const LayoutHeader = ({children}) => {
   return (
