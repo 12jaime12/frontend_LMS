6ac5575076bc29b099f093fc1970b20c290430d6
Laura Martínez <you@example.com>
2023-06-24 17:04:02 +0200

    dashboard sin vh

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -27,7 +27,7 @@ const Dashboard = () => {
           <li>
             <LayoutFlex direction="row" color="white" padding="3rem" gap="10rem">
               
-            <LayoutForm direction = "column" color="#f2f2f2" width="50vw"  padding = "2rem" gap="1rem">
+            <LayoutForm direction = "column" color="#f2f2f2" width="40vw" heigth="50vh" padding = "3rem" gap="1rem">
                 <H2C text="Conócenos" width="pequeño"/>
                 <PC text="Lengedary Motorsport es una comunidad creada para reunir a los
                   amantes de los coches y, además, encontrar diferentes
@@ -46,8 +46,8 @@ const Dashboard = () => {
           
           <li className="li-catalogo">
           <LayoutFlex direction="row" color="white" padding="3rem" gap="10rem">
-            <figure>
-              <LayoutAbsolute color="transparent" direction= "row" zindex="2" top="220vh" bottom="0" right="0" left="0">
+            
+              <LayoutAbsolute color="transparent" direction= "row" zindex="2" top="130rem" bottom="0" right="0" left="0">
                 {/* <div> */}
                
                   <img
@@ -62,7 +62,7 @@ const Dashboard = () => {
 
                 {/* <div className="Catalogo"> */}
                 <LayoutRelative color="transparent" margin="150px" zindex="3" left="350px" right="0" bottom="100px" top="0">
-                <LayoutForm direction = "column" color="#f2f2f2" width="40vw"  padding = "2rem" gap="1rem">
+                <LayoutForm direction = "column" color="#f2f2f2" width="40vw" heigth="50vh" padding = "3rem" gap="1rem">
                   <H2C text="Catálogo" width="pequeño"/>
                   <PC text="Podrás descubrir los últimos modelos de nuestras marcas colaboradoras y personalizar tu coche en tiempo real."
                   width="extralargo"></PC>
@@ -76,15 +76,15 @@ const Dashboard = () => {
                   </LayoutForm>
                 {/* </div> */}
                 </LayoutRelative>
-            </figure>
+            
             </LayoutFlex>
           </li>
           <li style={{margin: "13rem"}}> 
-          <LayoutFlex direction="row" color="white" padding="3rem" gap="10rem">
-           <figure>
-            <LayoutAbsolute color="#f2f2f2" direction="column" zindex="80"  top="260vh" bottom="0" right="40vh" left="50px">
+          <LayoutFlex direction="row" color="white" padding="4rem" gap="10rem">
+          
+            <LayoutAbsolute color="#f2f2f2" direction="column" zindex="80"  top="180rem" bottom="0" right="0" left="200px">
             {/* <div className="Comprayvende" style={{position:"absolute"}}> */}
-            <LayoutForm direction = "column" zindex="5" color="#f2f2f2" width="50vw"  padding = "2rem" gap="1rem">
+            <LayoutForm direction = "column" zindex="5" color="#f2f2f2" width="40vw" heigth="50vh" padding = "3rem" gap="1rem">
               <H2C text="Compra y vende" width="medio"/>
               <PC text="¿Necesitas vender tu coche?¿Estás en busca de un coche de
                 segunda mano? Esta es tu sección. Aquí encontrarás lo que
@@ -100,7 +100,7 @@ const Dashboard = () => {
             </LayoutForm>
             {/* </div> */}
             </LayoutAbsolute>
-            <LayoutRelative color="none" margin="0" zindex="4" left="300px" right="0" bottom="0" top="0">
+            <LayoutRelative color="none" margin="0" zindex="4" left="400px" right="0" bottom="0" top="0">
             <img
               src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1687246794/993409168-0_dmj2q2.jpg"
               alt="Compra y vende"
@@ -108,12 +108,12 @@ const Dashboard = () => {
               // style={{position: "relative", left: "300px", bottom: "100px"}}
             />
             </LayoutRelative>
-            </figure>
+           
             </LayoutFlex>
           </li>
           <li> 
           <LayoutFlex direction="row" color="white" padding="3rem" gap="10rem">
-          <LayoutAbsolute color="none" direction="row" zindex="2"  top="370vh" bottom="0" right="0" left="0">
+          <LayoutAbsolute color="none" direction="row" zindex="2"  top="240rem" bottom="0" right="0" left="0">
             <img
               src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1687246794/25b4391b79de1d76fb565d9a92c406a33c6b2a93d040761dca90c7163699c6a41d552b001665594e936a882820e733b055bd5da60cc0f8e45d4d9b_1280_qvgp7l.jpg"
               alt="Taller"
@@ -122,7 +122,7 @@ const Dashboard = () => {
            </LayoutAbsolute>
             {/* <div className="Taller"> */}
             <LayoutRelative color="#transparent" margin="0" zindex="3" left="400px" right="0" bottom="" top="">
-            <LayoutForm direction = "column" color="#f2f2f2" width="40vw"  padding = "2rem" gap="1rem">
+            <LayoutForm direction = "column" color="#f2f2f2" width="40vw" heigth="50vh" padding = "3rem" gap="1rem">
             <H2C text="Taller" width="pequeño"/>
               <PC text="Accede a nuestra red de Talleres asociados. Seleccionados por
                 sus servicios de la mayor calidad y cercanía para dejar tu coche
@@ -141,7 +141,7 @@ const Dashboard = () => {
           </li>
         </ul>
         
-        <LayoutInline gap="1rem" padding="5rem">    
+        <LayoutInline padding="5rem" gap="1rem" margin="10rem" >    
         <LayoutForm direction = "column" color="#f2f2f2" width="300px"  padding = "2rem" gap="1rem">
               <img
                 src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1687255176/gift_vouwzw.png"
@@ -169,8 +169,7 @@ const Dashboard = () => {
                 className="imgiconos"
               />
               <H2C text="Calidad" width="largo"></H2C>
-              <PC text="Garantía de calidad en todos nuestros servicios. Gracias a la
-                confianza de los usuarios." width="largo"></PC>
+              <PC text="Garantía de calidad en todos nuestros servicios. Gracias a nuestros proveedores." width="largo"></PC>
           </LayoutForm>
          </LayoutInline>
        </div>
