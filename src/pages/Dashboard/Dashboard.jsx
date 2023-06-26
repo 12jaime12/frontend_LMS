8cf58139e2d0c615446cfd35fb02a75fa684f8ca
Laura Martínez <you@example.com>
2023-06-26 19:53:15 +0200

    dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -24,45 +24,29 @@ const Dashboard = () => {
         /> 
         <ul  className="nav">
           {/*--------------- CONTAINER LOGO------------------------------- */}
-          <li>
-            <LayoutFlex direction="row" color="white" padding="3rem" gap="10rem">
-              
-            <LayoutForm direction = "column" color="#f2f2f2" width="40vw" heigth="50vh" padding = "3rem" gap="1rem">
+          <li className="li-conocenos">
+            <div className="boxConocenos">
                 <H2C text="Conócenos" width="pequeño"/>
                 <PC text="Lengedary Motorsport es una comunidad creada para reunir a los
                   amantes de los coches y, además, encontrar diferentes
                   funcionalidades." width="extralargo"></PC>
                 <Button type="submit" text="Saber más" variant="contained" color="white"/>
-                </LayoutForm>
-              
+              </div>
               <img
                 src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1686737065/logoredondo-removebg-preview_uidph4.png"
                 alt="segundoLogo"
                 className="Img-Dashboard ConocenosImg"
               />
-            </LayoutFlex>
-            
           </li>
-          
           <li className="li-catalogo">
-          <LayoutFlex direction="row" color="white" padding="3rem" gap="10rem">
-            
-              <LayoutAbsolute color="transparent" direction= "row" zindex="2" top="130rem" bottom="0" right="0" left="0">
-                {/* <div> */}
-               
                   <img
                     src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1687246794/7caf41ae44f772efd62df44450c72947d871dea699073065a3ef8367b7f006ba5bfa37dc66dbf7ec896d300ba8f69ac31578455787ae89322ba47c_1280_bcan6x.jpg"
                     alt="catalogo"
                     className="Img-Dashboard CatalogoImg"
                     // style={{position: "absolute"}}
                   />
-              
-                {/* </div> */}
-                </LayoutAbsolute>
 
-                {/* <div className="Catalogo"> */}
-                <LayoutRelative color="transparent" margin="150px" zindex="3" left="350px" right="0" bottom="100px" top="0">
-                <LayoutForm direction = "column" color="#f2f2f2" width="40vw" heigth="50vh" padding = "3rem" gap="1rem">
+                <div className="boxCatalogo"> 
                   <H2C text="Catálogo" width="pequeño"/>
                   <PC text="Podrás descubrir los últimos modelos de nuestras marcas colaboradoras y personalizar tu coche en tiempo real."
                   width="extralargo"></PC>
@@ -73,18 +57,10 @@ const Dashboard = () => {
                   variant="contained"
                   color="white"
                   />
-                  </LayoutForm>
-                {/* </div> */}
-                </LayoutRelative>
-            
-            </LayoutFlex>
+                </div>
           </li>
-          <li style={{margin: "13rem"}}> 
-          <LayoutFlex direction="row" color="white" padding="4rem" gap="10rem">
-          
-            <LayoutAbsolute color="#f2f2f2" direction="column" zindex="80"  top="180rem" bottom="0" right="0" left="200px">
-            {/* <div className="Comprayvende" style={{position:"absolute"}}> */}
-            <LayoutForm direction = "column" zindex="5" color="#f2f2f2" width="40vw" heigth="50vh" padding = "3rem" gap="1rem">
+          <li className="li-comprayvende">
+            <div className="boxComprayvende" >
               <H2C text="Compra y vende" width="medio"/>
               <PC text="¿Necesitas vender tu coche?¿Estás en busca de un coche de
                 segunda mano? Esta es tu sección. Aquí encontrarás lo que
@@ -97,32 +73,21 @@ const Dashboard = () => {
               variant="contained"
               color="white"
             />
-            </LayoutForm>
-            {/* </div> */}
-            </LayoutAbsolute>
-            <LayoutRelative color="none" margin="0" zindex="4" left="400px" right="0" bottom="0" top="0">
+            </div>
             <img
               src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1687246794/993409168-0_dmj2q2.jpg"
               alt="Compra y vende"
               className="Img-Dashboard comprayvendeImg"
               // style={{position: "relative", left: "300px", bottom: "100px"}}
             />
-            </LayoutRelative>
-           
-            </LayoutFlex>
           </li>
-          <li> 
-          <LayoutFlex direction="row" color="white" padding="3rem" gap="10rem">
-          <LayoutAbsolute color="none" direction="row" zindex="2"  top="240rem" bottom="0" right="0" left="0">
+          <li className="li-taller"> 
             <img
               src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1687246794/25b4391b79de1d76fb565d9a92c406a33c6b2a93d040761dca90c7163699c6a41d552b001665594e936a882820e733b055bd5da60cc0f8e45d4d9b_1280_qvgp7l.jpg"
               alt="Taller"
               className="Img-Dashboard tallerImg"
             />
-           </LayoutAbsolute>
-            {/* <div className="Taller"> */}
-            <LayoutRelative color="#transparent" margin="0" zindex="3" left="400px" right="0" bottom="" top="">
-            <LayoutForm direction = "column" color="#f2f2f2" width="40vw" heigth="50vh" padding = "3rem" gap="1rem">
+            <div className="boxTaller"> 
             <H2C text="Taller" width="pequeño"/>
               <PC text="Accede a nuestra red de Talleres asociados. Seleccionados por
                 sus servicios de la mayor calidad y cercanía para dejar tu coche
@@ -134,15 +99,11 @@ const Dashboard = () => {
                 variant="contained"
                 color="white"
               />
-              </LayoutForm>
-              </LayoutRelative>
-            {/* </div> */}
-            </LayoutFlex>
+            </div>
           </li>
         </ul>
-        
-        <LayoutInline padding="5rem" gap="1rem" margin="10rem" >    
-        <LayoutForm direction = "column" color="#f2f2f2" width="300px"  padding = "2rem" gap="1rem">
+        </div>
+        {/* <div className="oportunidades">
               <img
                 src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1687255176/gift_vouwzw.png"
                 alt="Oportunidades"
@@ -151,8 +112,8 @@ const Dashboard = () => {
               <H2C text="Oportunidades" width="largo"></H2C>
               <PC text="Las mejores opciones en servicio, calidad y precio, para nuestros
                 clientes." width="largo"></PC>
-         </LayoutForm>   
-         <LayoutForm direction = "column" color="#f2f2f2" width="300px" padding = "2rem" gap="1rem">
+                </div>
+                <div className="diseño">
               <img
                 src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1687255174/cube_yddwhl.png"
                 alt="Diseño"
@@ -161,8 +122,8 @@ const Dashboard = () => {
               <H2C text="Diseño" width="largo"></H2C>
               <PC text="Página intuitiva y fácil de usar para ofrecer una experiencia
                 óptima." width="largo"></PC>
-          </LayoutForm>
-          <LayoutForm direction = "column" color="#f2f2f2"width="300px" padding = "2rem" gap="1rem">
+   </div>
+   <div className="calidad">
               <img
                 src="https://res.cloudinary.com/dx3e6knoz/image/upload/v1687255172/medal-2_jpep79.png"
                 alt="Calidad"
@@ -170,9 +131,9 @@ const Dashboard = () => {
               />
               <H2C text="Calidad" width="largo"></H2C>
               <PC text="Garantía de calidad en todos nuestros servicios. Gracias a nuestros proveedores." width="largo"></PC>
-          </LayoutForm>
-         </LayoutInline>
-       </div>
+              </div> */}
+      
+       
      
     </>
   );
