ef59163d6df988af93605a6d34dca2bb8c007d89
12jaime12 <12jaimejcc12@gmail.com>
2023-06-26 13:02:19 +0200

    dia 7

--- src/service/API_proyect/comentario.service.js
+++ src/service/API_proyect/comentario.service.js
@@ -2,7 +2,11 @@ import { updateToken } from "../../util/updateToken";
 import { API } from "./service.config";
 
 export const createComentario = async (formData) => {
-  return API.post("comentarios/create", formData)
+  return API.post("comentarios/create", formData, {
+    headers: {
+      Authorization: `Bearer ${updateToken()}`,
+    },
+  })
     .then((res) => res)
     .catch((error) => {
       return error;
